baaeda98441e2b73c8591341a6598554818d31cf
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-20 15:19:26 -0700

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### PyCon 2017 - Building a Data Pipeline with Luigi
-### https://github.com/iamaaronknight/luigi-exercise-template
-### http://pyvideo.org/pycon-us-2017/build-a-data-pipeline-with-luigi.html
+#### https://github.com/iamaaronknight/luigi-exercise-template
+#### http://pyvideo.org/pycon-us-2017/build-a-data-pipeline-with-luigi.html
--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,17 @@
 #!/usr/local/bin/python3
 import luigi
+from time import sleep
 
 
 class HelloTask(luigi.Task):
     def run(self):
+        sleep(60)
         with open('hello.txt', 'w') as hello_file:
             hello_file.write('Hello')
             hello_file.close()
+
+    def output(self):
+        return luigi.LocalTarget('hello.txt')
 
 
 if __name__ == '__main__':
