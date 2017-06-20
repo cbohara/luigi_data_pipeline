d1bf8dfa7f376e95a838a9593e5292bc01e61cc3
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-20 16:07:17 -0700

    seperating out intro assignment

---
+++ hello_world/hello_world.py
@@ -0,0 +1,51 @@
+#!/usr/local/bin/python3
+import luigi
+from time import sleep
+
+
+class HelloTask(luigi.Task):
+    def run(self):
+        sleep(60)
+        with open('hello.txt', 'w') as hello_file:
+            hello_file.write('Hello')
+
+    def output(self):
+        return luigi.LocalTarget('hello.txt')
+
+
+class WorldTask(luigi.Task):
+    def run(self):
+        sleep(30)
+        with open('world.txt', 'w') as world_file:
+            world_file.write('World')
+
+    def output(self):
+        return luigi.LocalTarget('world.txt')
+
+
+class HelloWorldTask(luigi.Task):
+    id = luigi.Parameter(default='test')
+
+    def run(self):
+        sleep(60)
+        with open('hello.txt', 'r') as hello_file:
+            hello = hello_file.read()
+        with open('world.txt', 'r') as world_file:
+            world = world_file.read()
+        with open('hello_world.txt', 'w') as output_file:
+            content = '{} {}'.format(hello, world)
+            output_file.write(content)
+
+    def requires(self):
+        return [
+            HelloTask(path='results/{}/hello.txt'.format(self.id)),
+            WorldTask(path='results/{}/world.txt'.format(self.id)),
+        ]
+
+    def output(self):
+        path = 'results/{}/hello_world.txt'.format(self.id)
+        return luigi.LocalTarget(path)
+
+
+if __name__ == '__main__':
+    luigi.run()
