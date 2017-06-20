c91c5121183bbf7c7dd11c89a4fb62661d919d2a
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-20 15:39:46 -0700

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ### PyCon 2017 - Building a Data Pipeline with Luigi
 #### https://github.com/iamaaronknight/luigi-exercise-template
 #### http://pyvideo.org/pycon-us-2017/build-a-data-pipeline-with-luigi.html
+
+- How to run hello_world.py
+    - start daemon
+        - luigid --background --logdir=./logs --port=8082
+    - run application
+        - python3 hello_world.py HelloWorldTask --workers=2
--- hello_world.py
+++ hello_world.py
@@ -8,10 +8,37 @@ class HelloTask(luigi.Task):
         sleep(60)
         with open('hello.txt', 'w') as hello_file:
             hello_file.write('Hello')
-            hello_file.close()
 
     def output(self):
         return luigi.LocalTarget('hello.txt')
+
+
+class WorldTask(luigi.Task):
+    def run(self):
+        sleep(30)
+        with open('world.txt', 'w') as world_file:
+            world_file.write('World')
+
+    def output(self):
+        return luigi.LocalTarget('world.txt')
+
+
+class HelloWorldTask(luigi.Task):
+    def run(self):
+        sleep(60)
+        with open('hello.txt', 'r') as hello_file:
+            hello = hello_file.read()
+        with open('world.txt', 'r') as world_file:
+            world = world_file.read()
+        with open('hello_world.txt', 'w') as output_file:
+            content = '{} {}'.format(hello, world)
+            output_file.write(content)
+
+    def requires(self):
+        return [HelloTask(), WorldTask()]
+
+    def output(self):
+        return luigi.LocalTarget('hello_world.txt')
 
 
 if __name__ == '__main__':
